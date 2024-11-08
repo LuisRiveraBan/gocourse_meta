9164d18b5f606f33703352dad7d1357bf0022edb
Luis Rivera Ordinola <159965933+LuisRiveraBan@users.noreply.github.com>
2024-11-08 00:03:53 -0500

    Update meta.go

--- meta/meta.go
+++ meta/meta.go
@@ -1,30 +1,28 @@
 package meta
 
 import (
-	"os"
 	"strconv"
 )
 
 type Meta struct {
-	TotalCount int `json:"total_count"`
 	Page       int `json:"page"`
 	PerPage    int `json:"per_page"`
 	PageCount  int `json:"page_count"`
+	TotalCount int `json:"total_count"`
 }
 
-func NewMeta(page, perPage, total int, pagLimDef string) (*Meta, error) {
+func New(page, perPage, total int, pagLimDef string) (*Meta, error) {
 
 	if perPage <= 0 {
 		var err error
-		perPage, err = strconv.Atoi(os.Getenv(pagLimDef))
-
+		perPage, err = strconv.Atoi(pagLimDef)
 		if err != nil {
 			return nil, err
 		}
+
 	}
 
 	pageCount := 0
-
 	if total >= 0 {
 		pageCount = (total + perPage - 1) / perPage
 		if page > pageCount {
@@ -40,11 +38,12 @@ func NewMeta(page, perPage, total int, pagLimDef string) (*Meta, error) {
 		Page:       page,
 		PerPage:    perPage,
 		TotalCount: total,
+		PageCount:  pageCount,
 	}, nil
 }
 
-func (o *Meta) Offset() int {
-	return (o.Page - 1) * o.PerPage
+func (p *Meta) Offset() int {
+	return (p.Page - 1) * p.PerPage
 }
 
 func (p *Meta) Limit() int {
